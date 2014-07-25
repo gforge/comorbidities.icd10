08ca7e1793b3e420e5d640bad57dfff32527125c
unknown <ortmxgn@DS30517.sll.ds.sll.se>
2014-07-25 12:03:03 +0200

    Cont. 1 implementing Swedish ICD-9

--- R/cmrbdt.finder.regex.RCS_charlson.R
+++ R/cmrbdt.finder.regex.RCS_charlson.R
@@ -8,94 +8,152 @@
 #' @examples
 #' cmrbdt.finder.regex.charlson_Armitage2010("I252")
 cmrbdt.finder.regex.charlson_Armitage2010 <- 
-  function(icd_codes, 
-           out,
-           country_code,
-           include_acute = rep(TRUE, times=length(icd_codes)),
-           icd_ver = rep(FALSE, times=length(icd_codes))){
-  
-  # Based on Armitage JN, van der Meulen JH. 
-  # -- Identifying co-morbidity in surgical patients using 
-  # --- administrative data with the Royal College of Surgeons Charlson Score. 
-  # -- British Journal of Surgery. 2010 Maj 1;97(5):772-781. 
-  
-  rcs_charlsons <- list()
-  rcs_charlsons[['MI']] <- 
-    list(icd10 = c('^I2([123]|52)'),
-         icd9 = c('^41[02]'))
-  
-  rcs_charlsons[['CHF']] <- 
-    list(icd10 = c('^I1[13]', '^I255', '^I4[23]', '^I50', '^I517'),
-         icd9 = c('^39891', '^402(01|11|91)', '^404(01|03|[19][13])', '^42(5[456789]|8)'))
-  
-  rcs_charlsons[['PVD']] <- 
-    list(icd10 = c('^I7[0123]', '^I77[01]', '^K55[189]', '^R02', '^Z95[89]'),
-         icd9 = c('^0930', '^4373', '^44([01]|3[123456789]|71)', '^557[19]', '^V434'))
-  
-  rcs_charlsons[['CEVD']] <- 
-    list(icd10 = c('^G4[56]', '^I6'),
-         icd9 = c('^36234', '^43[012345678]'))
-  
-  rcs_charlsons[['DEM']] <- 
-    list(icd10 = c('^A810','^F0([0123]|51)', '^G3[01]'),
-         icd9 = c('^29(0|41)', '^3312'))
-  
-  rcs_charlsons[['COPD']] <- 
-    list(icd10 = c('^I26', '^I27', '^J4[01234567]', '^J6([01234567]|84)', '^J70[13]'),
-         icd9 = c('^416[89]', '^49', '^50([012345]|64|8[18])'))
-  
-  rcs_charlsons[['Rheum']] <- 
-    list(icd10 = c('^M0[569]', '^M120', '^M3(15|[23456])'),
-         icd9 = c('^4465', '^71(0[01234]|4[0128])', '^725'))
-  
-  rcs_charlsons[['LD']] <- 
-    list(icd10 = c('^B18', '^I85', '^I864', '^I982', '^K7([01]|2[19]|6)', '^R162', '^Z944'),
-         icd9 = c('^070([23][23]|[45]4|[69])', '^456[012]', '^572[2345678]', '^57([01]|3[3489])', '^V427'))
-  
-  rcs_charlsons[['DIAB']] <- 
-    list(icd10 = c('^E1[01234]'),
-         icd9 = c('^250'))
-  
-  rcs_charlsons[['PARA']] <- 
-    list(icd10 = c('^G114', '^G8[123]'),
-         icd9 = c('^3341', '^34([23]|4[01234569])'))
-  
-  rcs_charlsons[['RD']] <- 
-    list(icd10 = c('^I1[23]', '^N0[13578]', '^N1(7[12]|[89])', '^N25', '^Z49', '^Z940', '^Z992'),
-         icd9 = c('^403[019]1', '^404[019][23]', '^58(2|3[01234567]|[56]|80)', '^V4(20|51)', '^V56'))
-  
-  rcs_charlsons[['CANCER']] <- 
-    list(icd10 = c('^C[01]', '^C2[0123456]', '^C3[01234789]', '^C4[01356789]', '^C5[012345678]', '^C6', '^C7[0123456]', '^C8[0123458]', '^C9[01234567]'),
-         icd9 = c('^1[456]', '^17[012456789]', '^18', '^19[012345]', '^20[012345678]', '^2386'))
-  
-  rcs_charlsons[['METASTASIS']] <- 
-    list(icd10 = c('^C7[789]'),
-         icd9 = c( '^19[6789]'))
-  
-  rcs_charlsons[['HIV']] <- 
-    list(icd10 = c('^B2[0124]'),
-         icd9 = switch(country_code,
-                       US = c('^04[234]'),
-                       SE = c('279K')))
+  local({
+    # Based on Armitage JN, van der Meulen JH. 
+    # -- Identifying co-morbidity in surgical patients using 
+    # --- administrative data with the Royal College of Surgeons Charlson Score. 
+    # -- British Journal of Surgery. 2010 Maj 1;97(5):772-781. 
+    
+    rcs_charlsons <- list(US = list())
+    rcs_charlsons$US[['MI']] <- 
+      list(icd10 = c('^I2([123]|52)'),
+           icd9 = c('^41[02]'))
+    
+    rcs_charlsons$US[['CHF']] <- 
+      list(icd10 = c('^I1[13]', '^I255', '^I4[23]', '^I50', '^I517'),
+           icd9 = c('^39891', '^402(01|11|91)', '^404(01|03|[19][13])', '^42(5[456789]|8)'))
+    
+    rcs_charlsons$US[['PVD']] <- 
+      list(icd10 = c('^I7[0123]', '^I77[01]', '^K55[189]', '^R02', '^Z95[89]'),
+           icd9 = c('^0930', '^4373', '^44([01]|3[123456789]|71)', '^557[19]', '^V434'))
+    
+    rcs_charlsons$US[['CEVD']] <- 
+      list(icd10 = c('^G4[56]', '^I6'),
+           icd9 = c('^36234', '^43[012345678]'))
+    
+    rcs_charlsons$US[['DEM']] <- 
+      list(icd10 = c('^A810','^F0([0123]|51)', '^G3[01]'),
+           icd9 = c('^29(0|41)', '^3312'))
+    
+    rcs_charlsons$US[['COPD']] <- 
+      list(icd10 = c('^I26', '^I27', '^J4[01234567]', '^J6([01234567]|84)', '^J70[13]'),
+           icd9 = c('^416[89]', '^49', '^50([012345]|64|8[18])'))
+    
+    rcs_charlsons$US[['Rheum']] <- 
+      list(icd10 = c('^M0[569]', '^M120', '^M3(15|[23456])'),
+           icd9 = c('^4465', '^71(0[01234]|4[0128])', '^725'))
+    
+    rcs_charlsons$US[['LD']] <- 
+      list(icd10 = c('^B18', '^I85', '^I864', '^I982', '^K7([01]|2[19]|6)', '^R162', '^Z944'),
+           icd9 = c('^070([23][23]|[45]4|[69])', '^456[012]', '^572[2345678]', '^57([01]|3[3489])', '^V427'))
+    
+    rcs_charlsons$US[['DIAB']] <- 
+      list(icd10 = c('^E1[01234]'),
+           icd9 = c('^250'))
+    
+    rcs_charlsons$US[['PARA']] <- 
+      list(icd10 = c('^G114', '^G8[123]'),
+           icd9 = c('^3341', '^34([23]|4[01234569])'))
+    
+    rcs_charlsons$US[['RD']] <- 
+      list(icd10 = c('^I1[23]', '^N0[13578]', '^N1(7[12]|[89])', '^N25', '^Z49', '^Z940', '^Z992'),
+           icd9 = c('^403[019]1', '^404[019][23]', '^58(2|3[01234567]|[56]|80)', '^V4(20|51)', '^V56'))
+    
+    rcs_charlsons$US[['CANCER']] <- 
+      list(icd10 = c('^C[01]', '^C2[0123456]', '^C3[01234789]', '^C4[01356789]', '^C5[012345678]', '^C6', '^C7[0123456]', '^C8[0123458]', '^C9[01234567]'),
+           icd9 = c('^1[456]', '^17[012456789]', '^18', 
+                    '^19[012345678]', '^20[012345678]', 
+                    '^2386'))
+    
+    rcs_charlsons$US[['METASTASIS']] <- 
+      list(icd10 = c('^C7[789]'),
+           icd9 = c( '^19([678]|91)'))
+    
+    rcs_charlsons$US[['HIV']] <- 
+      list(icd10 = c('^B2[0124]'),
+           icd9 = c('^04[234]'))
+    
+    # Add some special cases for Swedish ICD
+    rcs_charlsons$SE <- rcs_charlsons$US
+    
+    # MI - no change
+    
+    rcs_charlsons$SE$CHF$icd9 <- 
+      c(# Not with heart failure in Swedish system
+        #'^3989', 
+        # '^402(01|11|91)', 
+        # '^404(01|03|[19][13])',
+        '^42(5[456789]|8)')
+    
+    # PVD - no change
+    # CEVD 
+    rcs_charlsons$SE$CEVD$icd9 <-
+      c('^3623', '^43[012345678]')
+    
+    # DEM - no change
+    
+    rcs_charlsons$SE$COPD$icd9 <-
+      c('^416[89]', '^49', '^50([012345]|64|81)')
+    
+    # Rheum - no change
+    
+    # LD - no change
 
-  acute_icd_codes <- list(icd10= '^(I2[123]|J46|N17[12]|N19)',
-                          # Used the translator from the Swedish National Board of Healthe and Welfare (Socialstyrelsen)
-                          icd9 = '^(410|42(30|95|96|98)|4939|58[34][67]|5908|586|7919|5939)')
-  
-  # Speeds up only to check the codes that are possible
-  icd_ver = pr.get.icd.ver(icd_codes, icd_ver)
-  
-  # Get a correctly formatted output vector
-  out <- pr.get.out.vector(out, rcs_charlsons)
-  
-  # Do the actual test loop
-  out <- pr.regex.code.match(out = out, 
-                             icd_codes = icd_codes, 
-                             cmrbdt.finder.regex.comorbidity = rcs_charlsons,
-                             icd_ver =  icd_ver,
-                             include_acute = include_acute,
-                             cmrbdt.finder.regex.acute = acute_icd_codes)
-  
-  return(out)
-}
+    # DIAB - no change
+
+    # PARA - no change
+    
+    rcs_charlsons$SE$RD$icd9 <-
+      c('^403[019]', 
+        '^404[019]', 
+        '^58(2|3[01234567]|[56]|80)', '^V4(20|51)', '^V56')
+
+    rcs_charlsons$SE$CANCER$icd9 <-
+      c('^1[456]', '^17[012456789]', '^18', 
+        '^19[0123459]', '^20[012345678]', '^2386')
+    
+    rcs_charlsons$SE$METASTASIS$icd9 <- 
+      c( '^19[678]') # The 199 is malignant without localization
+    
+    # HIV is a completely different code in Sweden
+    rcs_charlsons$SE$HIV$icd9 <- c('279K')
+    
+    function(icd_codes, 
+             out,
+             country_code,
+             include_acute = rep(TRUE, times=length(icd_codes)),
+             icd_ver = rep(FALSE, times=length(icd_codes))){
+      
+      
+      
+      acute_icd_codes <- list(icd10= '^(I2[123]|J46|N17[12]|N19)',
+                              # Used the translator from the Swedish National Board of Healthe and Welfare (Socialstyrelsen)
+                              icd9 = '^(410|42(30|95|96|98)|4939|58[34][67]|5908|586|7919|5939)')
+      
+      # Speeds up only to check the codes that are possible
+      icd_ver = pr.get.icd.ver(icd_codes, icd_ver)
+      
+      # Get a correctly formatted output vector
+      out <- pr.get.out.vector(out, rcs_charlsons)
+      
+      if (!country_code %in% names(rcs_charlsons))
+        stop("The requested country code '", country_code ,"''",
+             " is not yet available for RCS.",
+             " The only country codes available are:",
+             " '", paste(names(rcs_charlsons), collapse="', '", "'"))
+             
+      search_codes <- rcs_charlsons[[country_code]]
+      
+      # Do the actual test loop
+      out <- pr.regex.code.match(out = out, 
+                                 icd_codes = icd_codes, 
+                                 cmrbdt.finder.regex.comorbidity = search_codes,
+                                 icd_ver =  icd_ver,
+                                 include_acute = include_acute,
+                                 cmrbdt.finder.regex.acute = acute_icd_codes)
+      
+      return(out)
+    }
+  })
+
 
--- R/cmrbdt.finder.regex.charlson.R
+++ R/cmrbdt.finder.regex.charlson.R
@@ -6,105 +6,171 @@
 #' @export
 #' @examples
 #' cmrbdt.finder.regex.charlson_Quan2005("I212")
-cmrbdt.finder.regex.charlson_Quan2005 <- function(icd_codes,
-                             out,
-                             country_code,
-                             include_acute = rep(TRUE, times=length(icd_codes)),
-                             icd_ver = rep(FALSE, times=length(icd_codes))){
+cmrbdt.finder.regex.charlson_Quan2005 <- 
+  local({
+    # Based on Quan et al 2005
+    charlsons_v2 <- list(US=list())
+    charlsons_v2$US[['MI']] <-
+      list(icd10 = c('^I2([12]|52)'),
+           icd9 = c('^41[02]'))
+    
+    charlsons_v2$US[['CHF']] <-
+      list(icd10 = c('^I099', '^I1(10|3[02])', '^I255', '^I4(2[056789]|3)', '^I50', '^P290'),
+           icd9 =  c('^39891', '^402(01|11|91)', '^404(01|03|[19][13])', '^42(5[456789]|8)'))
 
-  # Based on Quan et al 2005
-  charlsons_v2 <- list()
-  charlsons_v2[['MI']] <-
-    list(icd10 = c('^I2([12]|52)'),
-         icd9 = c('^41[02]'))
+    charlsons_v2$US[['PVD']] <-
+      list(icd10 = c('^I7([01]|3[189]|71|9[02])', '^K55[189]', '^Z95[89]'),
+           icd9 = c('^0930', '^4373', '^44([01]|3[123456789]|71)', '^557[19]', '^V434'))
+    
+    charlsons_v2$US[['CVD']] <-
+      list(icd10 = c('^G4[56]', '^H340', '^I6'),
+           icd9 = c('^36234', '^43[012345678]'))
+    
+    charlsons_v2$US[['DEMENTIA']] <-
+      list(icd10 = c('^F0([0123]|51)', '^G3(0|11)'),
+           icd9 = c('^29(0|41)', '^3312'))
+    
+    charlsons_v2$US[['COPD']] <-
+      list(icd10 = c('^I27[89]', '^J4[01234567]', '^J6([01234567]|84)', '^J70[13]'),
+           icd9 = c('^416[89]', '^49', '^50([012345]|64|8[18])'))
+    
+    charlsons_v2$US[['RHEUM']] <-
+      list(icd10 = c('^M0[56]', '^M3(15|[234]|5[13]|60)'),
+           icd9 = c('^4465', '^71(0[01234]|4[0128])', '^725'))
+    
+    charlsons_v2$US[['PUD']] <-
+      list(icd10 = c('^K2[5678]'),
+           icd9 = c('^53[1234]'))
+    
+    charlsons_v2$US[['MILD.LIVER']] <-
+      list(icd10 = c('^B18', '^K7(0[01239]|1[3457]|[34]|6[023489])', '^Z944'),
+           icd9 = c('^070([23][23]|[45]4|[69])', '^57([01]|3[3489])', '^V427'))
+    
+    charlsons_v2$US[['DM']] <-
+      list(icd10 = c('^E1[01234][01689]'),
+           icd9 = c('^250[012389]'))
+    
+    charlsons_v2$US[['DM.COMP']] <-
+      list(icd10 = c('^E1[01234][23457]'),
+           icd9 = c('^250[4567]'))
+    
+    charlsons_v2$US[['PLEGIA']] <-
+      list(icd10 = c('^G041', '^G114', '^G8(0[12]|[12]|3[012349])'),
+           icd9 = c('^3341', '^34([23]|4[01234569])'))
+    
+    charlsons_v2$US[['RENAL']] <-
+      list(icd10 = c('^I120', '^I131', '^N0(3[234567]|5[234567])', '^N1[89]', '^N250', '^Z49[012]', '^Z940', '^Z992'),
+           icd9 = c('^403[019]1', '^404[019][23]', '^58(2|3[01234567]|[56]|80)', '^V4(20|51)', '^V56'))
+    
+    charlsons_v2$US[['MALIGNANCY']] <-
+      list(icd10 = c('^C[01]', '^C2[0123456]', '^C3[01234789]', '^C4[01356789]', '^C5[012345678]', '^C6', '^C7[0123456]', '^C8[123458]', '^C9[01234567]'),
+           icd9 = c('^1[456]', '^17[012456789]', '^18', '^19[012345]', '^20[012345678]', '^2386'))
+    
+    charlsons_v2$US[['SEVERE.LIVER']] <-
+      list(icd10 = c('^I8(5[09]|64)', '^I982', '^K7(04|[12]1|29|6[567])'),
+           icd9 = c('^456[012]', '^572[2345678]'))
+    
+    charlsons_v2$US[['METASTASIS']] <-
+      list(icd10 = c('^C7[789]', '^C80'),
+           icd9 = c( '^19[6789]'))
+    
+    charlsons_v2$US[['HIV']] <-
+      list(icd10 = c('^B2[0124]'),
+           icd9 = switch(country_code,
+                         US = c('^04[234]'),
+                         SE = c('279K')))
+    
+    charlsons_v2$SE <- charlsons_v2$US
+    
+    # MI - no change
 
-  charlsons_v2[['CHF']] <-
-    list(icd10 = c('^I099', '^I1(10|3[02])', '^I255', '^I4(2[056789]|3)', '^I50', '^P290'),
-         icd9 = switch(country_code,
-                       US = c('^39891', '^402(01|11|91)', '^404(01|03|[19][13])', '^42(5[456789]|8)'),
-                       SE = c('^3989', '^402(01|11|91)', '^404(01|03|[19][13])', '^42(5[456789]|8)')))
-  charlsons_v2[['PVD']] <-
-    list(icd10 = c('^I7([01]|3[189]|71|9[02])', '^K55[189]', '^Z95[89]'),
-         icd9 = c('^0930', '^4373', '^44([01]|3[123456789]|71)', '^557[19]', '^V434'))
+    charlsons_v2$SE$CHF$icd9 <-
+      c(# Not with heart failure in Swedish system
+        #'^3989', 
+        # '^402(01|11|91)', 
+        # '^404(01|03|[19][13])',
+        '^42(5[456789]|8)')
+    
+    # PVD - no change
+    
+    charlsons_v2$SE$CVD$icd9 <-
+      c('^36234', '^43[012345678]')
+    
+    charlsons_v2$SE$DEMENTIA$icd9 <-
+      c('^29(0|41)', '^3312')
+    
+    # DEM - no change
+    
+    charlsons_v2$SE$COPD$icd9 <-
+      c('^416[89]', '^49', '^50([012345]|64|81)')
+    
+    # RHEUM - no change
+    
+    # PUD - no change
+    
+    # LD - no change
+    
+    # DM - no change
+    # DM.COMP - no change
+    
+    # PLEGIA - no change
+    
+    charlsons_v2$SE$MILD.LIVER <-
+      list(icd10 = c('^B18', '^K7(0[01239]|1[3457]|[34]|6[023489])', '^Z944'),
+           icd9 = c('^070([23][23]|[45]4|[69])', '^57([01]|3[3489])', '^V427'))
+    
+    charlsons_v2$SE$RENAL <-
+      list(icd10 = c('^I120', '^I131', '^N0(3[234567]|5[234567])', '^N1[89]', '^N250', '^Z49[012]', '^Z940', '^Z992'),
+           icd9 = c('^403[019]1', '^404[019][23]', '^58(2|3[01234567]|[56]|80)', '^V4(20|51)', '^V56'))
+    
+    charlsons_v2$SE$MALIGNANCY$icd9 <-
+      c('^1[456]', '^17[012456789]', '^18',
+        '^19[0123459]', '^20[012345678]', '^2386')
+    
+    # SEVERE.LIVER - no change
+    
+    charlsons_v2$SE$METASTASIS$icd9 <-
+      c('^19[678]')
+    
+    charlsons_v2$SE$HIV$icd9 <-
+      c('279K')
+    
+    
+    function(icd_codes,
+             out,
+             country_code,
+             include_acute = rep(TRUE, times=length(icd_codes)),
+             icd_ver = rep(FALSE, times=length(icd_codes))){
 
-  charlsons_v2[['CVD']] <-
-    list(icd10 = c('^G4[56]', '^H340', '^I6'),
-         icd9 = c('^36234', '^43[012345678]'))
-
-  charlsons_v2[['DEMENTIA']] <-
-    list(icd10 = c('^F0([0123]|51)', '^G3(0|11)'),
-         icd9 = c('^29(0|41)', '^3312'))
-
-  charlsons_v2[['COPD']] <-
-    list(icd10 = c('^I27[89]', '^J4[01234567]', '^J6([01234567]|84)', '^J70[13]'),
-         icd9 = c('^416[89]', '^49', '^50([012345]|64|8[18])'))
-
-  charlsons_v2[['RHEUM']] <-
-    list(icd10 = c('^M0[56]', '^M3(15|[234]|5[13]|60)'),
-         icd9 = c('^4465', '^71(0[01234]|4[0128])', '^725'))
-
-  charlsons_v2[['PUD']] <-
-    list(icd10 = c('^K2[5678]'),
-         icd9 = c('^53[1234]'))
-
-  charlsons_v2[['MILD.LIVER']] <-
-    list(icd10 = c('^B18', '^K7(0[01239]|1[3457]|[34]|6[023489])', '^Z944'),
-         icd9 = c('^070([23][23]|[45]4|[69])', '^57([01]|3[3489])', '^V427'))
-
-  charlsons_v2[['DM']] <-
-    list(icd10 = c('^E1[01234][01689]'),
-         icd9 = c('^250[012389]'))
-
-  charlsons_v2[['DM.COMP']] <-
-    list(icd10 = c('^E1[01234][23457]'),
-         icd9 = c('^250[4567]'))
-
-  charlsons_v2[['PLEGIA']] <-
-    list(icd10 = c('^G041', '^G114', '^G8(0[12]|[12]|3[012349])'),
-         icd9 = c('^3341', '^34([23]|4[01234569])'))
-
-  charlsons_v2[['RENAL']] <-
-    list(icd10 = c('^I120', '^I131', '^N0(3[234567]|5[234567])', '^N1[89]', '^N250', '^Z49[012]', '^Z940', '^Z992'),
-         icd9 = c('^403[019]1', '^404[019][23]', '^58(2|3[01234567]|[56]|80)', '^V4(20|51)', '^V56'))
-
-  charlsons_v2[['MALIGNANCY']] <-
-    list(icd10 = c('^C[01]', '^C2[0123456]', '^C3[01234789]', '^C4[01356789]', '^C5[012345678]', '^C6', '^C7[0123456]', '^C8[123458]', '^C9[01234567]'),
-         icd9 = c('^1[456]', '^17[012456789]', '^18', '^19[012345]', '^20[012345678]', '^2386'))
-
-  charlsons_v2[['SEVERE.LIVER']] <-
-    list(icd10 = c('^I8(5[09]|64)', '^I982', '^K7(04|[12]1|29|6[567])'),
-         icd9 = c('^456[012]', '^572[2345678]'))
-
-  charlsons_v2[['METASTASIS']] <-
-    list(icd10 = c('^C7[789]', '^C80'),
-         icd9 = c( '^19[6789]'))
-
-  charlsons_v2[['HIV']] <-
-    list(icd10 = c('^B2[0124]'),
-         icd9 = switch(country_code,
-                       US = c('^04[234]'),
-                       SE = c('279K')))
-
-  acute_icd_codes <- list(icd10= '^(I2[123]|J46|N17[12]|N19)',
-                          # Used the translator from the Swedish National Board of Healthe and Welfare (Socialstyrelsen)
-                          icd9 = '^(410|42(30|95|96|98)|4939|58[34][67]|5908|586|7919|5939)')
-
-  # Speeds up only to check the codes that are possible
-  icd_ver <- pr.get.icd.ver(icd_codes, icd_ver)
-
-  # Get a correctly formatted output vector
-  out <- pr.get.out.vector(out, charlsons_v2)
-
-  # Do the actual test loop
-  out <- pr.regex.code.match(out = out,
-                             icd_codes = icd_codes,
-                             cmrbdt.finder.regex.comorbidity = charlsons_v2,
-                             icd_ver =  icd_ver,
-                             include_acute = include_acute,
-                             cmrbdt.finder.regex.acute = acute_icd_codes)
-
-  return(out)
-}
+      acute_icd_codes <- list(icd10= '^(I2[123]|J46|N17[12]|N19)',
+                              # Used the translator from the Swedish National Board of Healthe and Welfare (Socialstyrelsen)
+                              icd9 = '^(410|42(30|95|96|98)|4939|58[34][67]|5908|586|7919|5939)')
+      
+      # Speeds up only to check the codes that are possible
+      icd_ver <- pr.get.icd.ver(icd_codes, icd_ver)
+      
+      # Get a correctly formatted output vector
+      out <- pr.get.out.vector(out, charlsons_v2)
+      
+      if (!country_code %in% names(charlsons_v2))
+        stop("The requested country code '", country_code ,"''",
+             " is not yet available for RCS.",
+             " The only country codes available are:",
+             " '", paste(names(charlsons_v2), collapse="', '", "'"))
+      
+      search_codes <- charlsons_v2[[country_code]]
+      
+      # Do the actual test loop
+      out <- pr.regex.code.match(out = out,
+                                 icd_codes = icd_codes,
+                                 cmrbdt.finder.regex.comorbidity = search_codes,
+                                 icd_ver =  icd_ver,
+                                 include_acute = include_acute,
+                                 cmrbdt.finder.regex.acute = acute_icd_codes)
+      
+      return(out)
+    }
+  })
 
 
 #' @rdname cmrbdt.finder.regex
